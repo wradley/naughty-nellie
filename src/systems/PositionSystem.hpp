87a85d3a2672129b5cf56eec3a51e3d98f269d08
Whelan Callahan <wcallahan@wi.rr.com>
2017-02-10 16:28:33 -0600

    added position system

---
+++ src/systems/PositionSystem.hpp
@@ -0,0 +1,18 @@
+#ifndef __POSITION_SYSTEM_H___
+#define __POSITION_SYSTEM_H___
+
+namespace wj
+{
+    class PositionSystem
+    {
+    public:
+
+        PositionSystem();
+        PositionSystem(const PositionSystem &) = delete;
+        ~PositionSystem();
+
+    private:
+    };
+};
+
+#endif /* end of include guard: __POSITION_SYSTEM_H___ */
