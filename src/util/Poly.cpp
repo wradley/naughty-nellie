1874e9e8cae5aa642223aaed476b642799cb251b
Whelan Callahan <wcallahan@wi.rr.com>
2017-02-11 17:49:31 -0600

    Started on polygon class

--- src/util/Poly.cpp
+++ src/util/Poly.cpp
@@ -1,1 +1,61 @@
 #include "Poly.hpp"
+#include <cstdio>
+#include <cassert>
+
+wj::Poly::Poly(uint32_t num_verts) :
+_verts(nullptr), _num_verts(num_verts), _last_added_vert(0)
+{
+    _verts = new Vec2[_num_verts];
+}
+
+
+wj::Poly::Poly(const Poly &other) :
+_verts(nullptr), _num_verts(other._num_verts), _last_added_vert(0)
+{
+    _verts = new Vec2[_num_verts];
+
+    for (int i = 0; i < _num_verts; ++i)
+    {
+        _verts[i] = other._verts[i];
+    }
+}
+
+
+void wj::Poly::operator= (const Poly &other)
+{
+    _last_added_vert = 0;
+    _num_verts = other._num_verts;
+    if (_verts != nullptr) delete [] _verts;
+    _verts = new Vec2[_num_verts];
+
+    for (int i = 0; i < _num_verts; ++i)
+    {
+        _verts[i] = other._verts[i];
+    }
+}
+
+
+wj::Poly::~Poly()
+{
+    delete [] _verts;
+}
+
+
+void wj::Poly::add_vert(const Vec2 vert)
+{
+    if (_last_added_vert >= _num_verts - 1)
+    {
+        printf("Added too many verts to polygon.\n");
+        assert(0);
+    }
+
+    _verts[++_last_added_vert] = vert;
+}
+
+
+wj::Vec2 wj::Poly::min_translation(const Poly &other) const
+{
+    Vec2 v;
+    assert(0);
+    return v;
+}
--- src/util/Poly.hpp
+++ src/util/Poly.hpp
@@ -1,10 +1,32 @@
 #ifndef __POLY_H__
 #define __POLY_H__
 
+#include "Vec2.hpp"
+#include <cstdint>
+
 namespace wj
 {
     class Poly
-    {};
+    {
+    public:
+
+        Poly(uint32_t num_verts);
+        Poly(const Poly &other);
+        void operator= (const Poly &other);
+        ~Poly();
+
+        void add_vert(const Vec2 vert);
+
+        // minimum translation vector to not be colliding with another polygon
+        // - returns 0 if they're not colliding
+        Vec2 min_translation(const Poly &other) const;
+
+    private:
+
+        Vec2 *_verts;
+        uint32_t _num_verts, _last_added_vert;
+
+    };
 };
 
 #endif /* end of include guard: __POLY_H__ */
